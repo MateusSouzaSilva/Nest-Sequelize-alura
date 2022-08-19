37edf077ee031e00608779a0bf90e87fb43dd38e
Mateus Souza <mateus_souza11@hotmail.com.br>
2022-08-19 11:27:20 -0300

    finalizando o projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosController } from './produtos.controller';
-import { ProdutosService } from './produtos.service';
+import { Livro } from './livro.model';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    SequelizeModule.forFeature([Livro])
+  ],
   controllers: [AppController,
-  ProdutosController],
-  providers: [AppService, ProdutosService],
+  LivrosController],
+  providers: [AppService, LivrosService],
 })
 export class AppModule {}
